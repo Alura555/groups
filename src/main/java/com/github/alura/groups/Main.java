dc0466b1bce05a3bca62139b5cc237fa8da5f22f
Luiza Abdrashitova <abdrashitova.lr@gmail.com>
2024-02-12 15:32:28 +0300

    Add string grouping algorithm

--- src/main/java/com/github/alura/groups/Main.java
+++ src/main/java/com/github/alura/groups/Main.java
@@ -1,13 +1,172 @@
 package com.github.alura.groups;
 
-/**
- * Hello world!
- *
- */
-public class Main
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static final String STRING_PATTERN = "(\"\\d*\"(;|$))+";
+    public static final String OUTPUT_TXT = "output.txt";
+
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+
+        if (args.length == 0) {
+            System.out.println("Please provide the input file as an argument when running the program.");
+            System.out.println("Usage: java -jar groups-1.0-SNAPSHOT.jar <input_file>");
+            return;
+        }
+
+        String inputFile = args[0];
+
+        Set<String> uniqueLines;
+        try {
+            uniqueLines = getUniqueLines(inputFile);
+        } catch (IOException e) {
+            System.err.println("Error while reading the file: " + e.getMessage());
+            return;
+        }
+
+        List<List<String>> groups = groupLines(new ArrayList<>(uniqueLines));
+
+
+        try {
+            writeGroupsToFile(groups);
+        } catch (IOException e) {
+            System.err.println("Error while writing to the file " + e.getMessage());
+            return;
+        }
+
+        long endTime = System.nanoTime();
+        double timeElapsedSeconds = (endTime - startTime) / 1e9;
+        System.out.println("Number of groups with more than one element: " + groups.size());
+        System.out.printf("Total execution time: %.6f seconds%n", timeElapsedSeconds);
+    }
+
+    private static Set<String> getUniqueLines(String inputFile) throws IOException {
+        Set<String> uniqueLines = new HashSet<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.matches(STRING_PATTERN)) {
+                    String newLine = line.replace("\"", "");
+                    uniqueLines.add(newLine);
+                }
+            }
+        }
+        return uniqueLines;
+    }
+
+    private static void writeGroupsToFile(List<List<String>> groups) throws IOException {
+        try (PrintWriter pw = new PrintWriter(new FileWriter(OUTPUT_TXT))) {
+            AtomicInteger groupCount = new AtomicInteger(0);
+            groups.forEach(group -> {
+                pw.println("Group " + groupCount.incrementAndGet());
+                group.forEach(pw::println);
+            });
+        }
+    }
+
+    public static List<List<String>> groupLines(List<String> lines) {
+        Map<String, Map<Integer, Integer>> valuesCharacteristics = new HashMap<>();
+        List<List<String>> groupsList = new ArrayList<>();
+
+        for (String line : lines) {
+            String[] words = line.split(";");
+            Set<Integer> groupNumbers = new HashSet<>();
+            Map<String, Integer> newValues = new HashMap<>();
+            for (int i = 0; i < words.length; i++) {
+                if (words[i].isEmpty())
+                    continue;
+
+                if (valuesCharacteristics.containsKey(words[i])
+                        && valuesCharacteristics.get(words[i]).get(i) != null ) {
+                    groupNumbers.add(valuesCharacteristics.get(words[i]).get(i));
+                } else {
+                    newValues.put(words[i], i);
+                }
+            }
+
+            Integer currentLineGroup = getLineGroupNumber(groupsList, line, groupNumbers);
+
+            updateValueCharacteristics(valuesCharacteristics, newValues, currentLineGroup);
+        }
+
+        return groupsList
+                .stream()
+                .filter(Objects::nonNull)
+                .filter(x -> x.size() > 1)
+                .sorted((g1, g2) -> g2.size() - g1.size())
+                .collect(Collectors.toList());
+    }
+
+    private static Integer getLineGroupNumber(List<List<String>> groupsList, String line, Set<Integer> groupNumbers) {
+        Integer currentLineGroup;
+        if (!groupNumbers.isEmpty()) {
+            if (groupNumbers.size() > 1) {
+                currentLineGroup = mergeGroups(groupsList, line, groupNumbers);
+            } else {
+                currentLineGroup = addLineToGroup(groupsList, line, groupNumbers);
+            }
+        } else {
+            currentLineGroup = createNewGroup(groupsList, line);
+        }
+        return currentLineGroup;
+    }
+
+    private static void updateValueCharacteristics(Map<String, Map<Integer, Integer>> valuesCharacteristics,
+                                                   Map<String, Integer> newValues,
+                                                   Integer currentGroupNumber) {
+        newValues.keySet()
+                .forEach(value -> {
+                    Map<Integer, Integer> characteristics = valuesCharacteristics
+                            .computeIfAbsent(value, k -> new HashMap<>());
+                    characteristics.put(newValues.get(value), currentGroupNumber);
+                    valuesCharacteristics.put(value, characteristics);
+                });
+    }
+
+    private static Integer addLineToGroup(List<List<String>> groupsList,
+                                          String line,
+                                          Set<Integer> groupNumbers) {
+        Integer group = groupNumbers.iterator().next();
+        groupsList.get(group).add(line);
+        return group;
+    }
+
+    private static Integer createNewGroup(List<List<String>> groupsList,
+                                          String line) {
+        List<String> newGroup = new ArrayList<>();
+        newGroup.add(line);
+        groupsList.add(newGroup);
+        return groupsList.size() - 1;
+    }
+
+    private static Integer mergeGroups(List<List<String>> groupsList,
+                                       String line,
+                                       Set<Integer> groupNumbers) {
+        List<String> mergedGroup = new ArrayList<>();
+        for (Integer group : groupNumbers) {
+            List<String> groupLines = groupsList.get(group);
+            mergedGroup.addAll(groupLines);
+            groupsList.set(group, null);
+        }
+        mergedGroup.add(line);
+        groupsList.add(mergedGroup);
+        return groupsList.size() - 1;
     }
 }
